f5b9c5fc4d891267ca66b28efda347e77f9d3db6
keeze12 <momoru204@gmail.com>
2024-10-31 13:46:50 +0500

    Initial commit

---
+++ data/config.py
@@ -0,0 +1,6 @@
+THREADS = 1
+
+# rpc https://www.quicknode.com/login
+RPC_URL = 'https://grass.helius-rpc.com/?api-key=96bb60fe-c4e7-4fb5-8a7f-e02bb63ba823'
+DESTINATION_ADDRESS = 'поменяйте на свой адрес solana'
+
